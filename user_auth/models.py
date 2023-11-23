88127ddeadf8de5aae77010708bf9ccbe9491ece
AnByoungHyun <abhyuni@gmail.com>
2023-11-23 04:33:54 +0000

    회원 가입 기능 추가

--- user_auth/models.py
+++ user_auth/models.py
@@ -1,3 +1,4 @@
+from typing import Any
 from django.db import models
 from django.contrib.auth.models import AbstractUser, UserManager
 
@@ -6,10 +7,17 @@ class AccountManager(UserManager):
     '''
     사용자 정보를 관리하기 위한 기능을 커스텀 하기 위해 분리해 둠
     '''
-    pass
+    def create_user(self, username: str, email: str | None = ..., password: list | None = ..., **extra_fields: Any) -> Any:
+        if len(password) != 2:
+            return None
+        
+        if password[0] != password[1]:
+            return None
+        
+        return super().create_user(username, email, password[0], **extra_fields)
 
 class Account(AbstractUser):
     '''
     사용자 정보를 Django에서 제공하는 정보로 사용하지만 나중에 커스텀 할 수 있도록 분리해 둠
     '''
-    pass
+    objects = AccountManager()
--- user_auth/views.py
+++ user_auth/views.py
@@ -6,24 +6,24 @@ from django.contrib import auth
 from .models import Account
 
 class Login(View):
-    '''
+    """
     로그인을 위한 기능이 구현되어 있습니다.
-    '''
+    """
     def get(self, request: HttpRequest):
-        '''
+        """
         로그인 요청에 대해 관련 페이지를 제공합니다.
-        '''
+        """
         user = request.user
         
         if user.is_authenticated:
-            return render(request, 'user_auth/already_logined.html')
+            return render(request, "user_auth/already_logined.html")
         
         return render(request, "user_auth/login.html")
     
     def post(self, request: HttpRequest):
-        '''
+        """
         로그인 요청에 대해 이메일 주소와 패스워드를 확인하여 거부/승낙 처리를 합니다.
-        '''
+        """
         email = request.POST.get("email")
         password = request.POST.get("password")
         
@@ -34,22 +34,21 @@ class Login(View):
             return redirect("/")
         
         context = {
-            "email": email,
+            "data": request.POST,
             "valid": "is-invalid",
             "message": "로그인 실패! 이메일 주소와 패스워드를 다시 확인하세요.",
-            "data": request.POST
         }
         
         return render(request, "user_auth/login.html", context=context)
     
 class Logout(View):
-    '''
+    """
     로그아웃을 위한 기능이 구현되어 있습니다.
-    '''
+    """
     def get(self, request: HttpRequest):
-        '''
+        """
         로그아웃 요청에 대해 로그아웃 처리합니다.
-        '''
+        """
         user = request.user
         
         if user.is_authenticated:
@@ -57,26 +56,41 @@ class Logout(View):
             return redirect("/")
     
 class SignUp(View):
-    '''
+    """
     회원 가입을 위한 기능이 구현되어 있습니다.
-    '''
+    """
     def get(self, request: HttpRequest):
-        '''
+        """
         회원 가입 요청에 대해 관련 페이지를 제공합니다.
-        '''
+        """
         user = request.user
         
         if user.is_authenticated:
-            return render(request, 'user_auth/already_logined.html')
+            return render(request, "user_auth/already_logined.html")
         
         return render(request, "user_auth/signup.html")
     
     def post(self, request: HttpRequest):
-        '''
+        """
         회원 가입 요청에 대해 이메일 주소와 패스워드 등의 정보를 검증 후 거부/승낙 처리를 합니다.
-        '''
-        user = Account.objects
-        pass
+        """
+        email = request.POST.get("email")
+        password = request.POST.getlist("password")
+        first_name = request.POST.get("first_name")
+        last_name = request.POST.get("last_name")
+        
+        user = Account.objects.create_user(username=email, email=email, password=password, first_name=first_name, last_name=last_name)
+        
+        if not user:
+            context = {
+                "data": request.POST,
+                "valid": "is-invalid",
+                "message": "회원 가입 실패! 이메일 주소와 패스워드를 다시 확인하세요.",
+            }
+            return render(request, "user_auth/signup.html", context=context)
+        
+        return redirect("/")
+            
     
 class ChangePassword(View):
     def get(self, request: HttpRequest):
