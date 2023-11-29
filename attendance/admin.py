2cc9fbf9e371aa4b89c0b21b07689a82436cbc47
AnByoungHyun <abhyuni@gmail.com>
2023-11-29 01:07:48 +0000

    출석 모델 생성 및 관리자 페이지 등록

--- attendance/admin.py
+++ attendance/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import AttendanceModel
 
 # Register your models here.
+
+class AttendanceModelAdmin(admin.ModelAdmin):
+    # 관리자 리스트 뷰에서 표시할 필드
+    list_display = ('name', 'context')
+
+    # 리스트 뷰에서 필터 옵션으로 사용할 필드
+    list_filter = ('name',)
+
+    # 검색 기능에서 사용할 필드
+    search_fields = ['name', 'context']
+
+    # 필드 세트를 정의하여 입력 양식의 레이아웃을 커스터마이징할 수 있습니다.
+    fieldsets = (
+        (None, {
+            'fields': ('name', 'context')
+        }),
+    )
+
+# MyModel과 MyModelAdmin을 Django 관리자에 등록합니다.
+admin.site.register(AttendanceModel, AttendanceModelAdmin)
--- attendance/models.py
+++ attendance/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class AttendanceModel(models.Model):
+    name = models.CharField(max_length=50)   # 'name' 필드, 최대 길이 100자
+    context = models.CharField(max_length=500) # 'context' 필드, 최대 길이 500자
+
+    def __str__(self):
+        return self.name
