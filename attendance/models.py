d640a460784aebf06972e272779ef631cb637fd2
AnByoungHyun <abhyuni@gmail.com>
2023-11-29 05:58:18 +0000

    출석 데이터 저장 및 조회 기능 추가

--- attendance/models.py
+++ attendance/models.py
@@ -1,12 +1,26 @@
 from django.db import models
 
 # Create your models here.
+class AttendanceManager(models.Manager):
+    def add(self, name: str, context: str):
+        model = self.model(name=name, context=context)
+        model.save()
+        return model
+    
+    def get(self, name: str):
+        return super().get(name=name)
+    
+    def create(self, name: str, context: str):
+        return super().create(name=name, context=context)
+    
 class AttendanceModel(models.Model):
     name = models.CharField(max_length=50)   # 'name' 필드, 최대 길이 100자
     context = models.CharField(max_length=500) # 'context' 필드, 최대 길이 500자
+    
+    objects = AttendanceManager()
 
     def __str__(self):
-        return self.name
+        return f"({self.name}, {self.context})"
 
     class Meta:
         verbose_name = "출석"
--- attendance/views.py
+++ attendance/views.py
@@ -1,12 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpRequest, HttpResponse
 from django.views import View
+from .models import AttendanceModel
 
 # Create your views here.
 class Attendance(View):
 
     def get(self, request: HttpRequest) -> HttpResponse:
-        return render(request, "attendance/index.html")
+        data = AttendanceModel.objects.all()
+        
+        context = {
+            "data": data
+        }
+        
+        return render(request, "attendance/index.html", context=context)
     
     def post(self, request: HttpRequest) -> HttpResponse:
-        pass
+        name = request.POST.get("name")
+        context = request.POST.get("context")
+        
+        AttendanceModel.objects.create(name, context)
+        
+        return redirect("attendance:index")
